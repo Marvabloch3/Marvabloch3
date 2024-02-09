df49308a4f4fbc21bee74a9acbeee9db8ff74cf1
MARVA <159155517+Marvabloch3@users.noreply.github.com>
2024-02-09 20:57:55 +0530

    Add files via upload

---
+++ pattern .cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,num;
+    cout<<"enter num";
+    cin>>num;
+   
+    for(i=0;i<=num;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            cout<<"j"<<endl;
+        }
+    }
+    return 0;
+
+
+}
